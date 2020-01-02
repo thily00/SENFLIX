aa77eada67ecf0613674b453f2563788d9ee7429
thily00 <42220232+thily00@users.noreply.github.com>
2020-01-02 13:42:12 +0100

    Add files via upload

---
+++ data/serie.js
@@ -0,0 +1,39 @@
+export default series = [
+    {
+        id:1,
+        tittle:"Golden",
+        imageurl:'https://www.leral.net/photo/art/grande/32819693-30488335.jpg?v=1555598539',
+        description:'Jams revient au Sénégal sur injonction de sa mère Mage pour redresser Golden, une entreprise d’exploitation d’or au bord du déclin. Entouré d’ennemis qui convoitent sa place dont Fadel un ancien employé de Golden qui a juré de détruire Mage par tous les moyens,Jams lutte pour réussir sa mission. Alors que ses adversaires se multiplient, les démons du passé nébuleux de Jams ressurgissent pour régler des comptes avec lui.',
+    },
+    {
+        id:2,
+        tittle:'Idoles',
+        imageurl:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-KoCveNjbL4OgSNDxDaEHJzN66Baz20YKDgzuXanMrOhc3HTA&s',
+        description:'Dans « IDOLES » l’univers de la presse est un prétexte pour explorer l’humain. Il n’y a pas que des gens pourri d’un coté et de l’autre des méchants et corrompus, « La nature humaine est beaucoup plus complexe » comme l’écrivait feue Aminata Sophie Diéye (Ndéye Taxawalou) dans ce scénario. Toutes les personnes de cette série nous rappellent les gens que nous côtoyons chaque jour. Certains nous donnent l’impression de n’être que vertu et d’autre l’image du marginal ou de la trainée, mais lorsqu’on les fréquente, on découvre un autre monde parfois à l’opposé de nos convictions'
+
+    },
+    {
+        id:3,
+        tittle:'Maitresse d\'un homme marié',
+        imageurl:'https://www.marodi.tv/vignettesseries/maitresse-dun-homme-marie-1.jpg',
+        description:'La vie d’une femme peut bien cacher celle d’une autre, mais à chacune son histoire ! Préparez-vous à la détester, croyez-nous, que vous l’aimez ou pas c’est le cadet de ses soucis. Car elle assume qui elle est et ce qu’elle fait. Maréme Dial est la maitresse de Cheikh Diagne. Un statut qu’elle assume depuis qu’elle l’a accepté dans sa vie. Entre eux comme dans tout couple il y’a des hauts et des bas. Comme toute maîtresse elle aimerait bien connaitre l’autre, la femme légitime celle qui légalement est la maitresse des lieux. Quand par hasard le destin met sur son chemin Lalla Piem N’diaye, Maréme est décidée à jouer le jeu. « Maîtresse d’un homme marié » c’est aussi une immersion dans la vie de Djalika Sagna une mère de famille dont le mari irresponsable fait de sa vie un enfer. Racky Sow, vit une relation mère-fille très difficile. Dior Diop de par sa gaieté et sa franchise est notre coup de cœur. Des histoires de femmes qui rappellent notre vécu. Avec des épisodes qui mettent l’accent sur les divers défis auxquels les femmes d’aujourd’hui font face dans leur quotidien. La série est une véritable boite d’émotions : pleurs, rires, crises de jalousie, colère, tromperie, tendresse, tout est au rendez-vous pour vous inviter à entrer dans la sphère des femmes.'
+    },
+    {
+        id:4,
+        tittle:'Brigade des femmes',
+        imageurl:'https://klinfos.com/wp-content/uploads/2019/04/moeurs-la-brigade-des-femmes-1.jpg',
+        description:"MOEURS, la Brigade des Femmes .Anna, ex-lieutenant, recrute les membres de sa nouvelle brigade qui sera exclusivement féminine.Vivez les enquêtes d' Anna au côté de Collé, Imane, Noor,Yandé et Marly...Leur mission, lutter contre l'injustice. Bou kénn noppéti."
+    },
+    {
+        id:5,
+        tittle:'Pod et Marichou',
+        imageurl:'http://marodi.tv/vignettesseries/pod-et-marichou-1.jpg ',
+        description:'Pod et Marichou est une série qui retrace l\'histoire entre deux jeunes Sénégalais'
+    },
+    {
+        id:6,
+        tittle:'Adja',
+        imageurl:'https://www.marodi.tv/vignettesseries/adja-1.jpg',
+        description:'Adja est une sitcom produite par PATISEN. Diffusée pendant le Ramadan,elle raconte l\'histoire d\'une famille hors du commun.'
+    }
+]
